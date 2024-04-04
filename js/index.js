30ea53a8ba43e328ba31a2b730547e729e4a3221
Anas Laghrissi <108026572+anaslaghrissi@users.noreply.github.com>
2024-04-04 18:50:05 +0000

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var menu = document.getElementById("menu");
+var menu2 = document.getElementById("menu2");
+
+menu.onclick = function () {
+  menu.classList.toggle("openmenu");
+};
+menu2.onclick = function () {
+  menu2.classList.toggle("openmenu2");
+};
